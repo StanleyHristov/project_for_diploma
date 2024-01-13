e4e0793c8f9dcae952b090f4707c0dd980c728db
StanleyHristov <152205329+StanleyHristov@users.noreply.github.com>
2024-01-13 13:34:42 +0200

    Create alexnet.py

---
+++ automotive/Opencv files/alexnet.py
@@ -0,0 +1,37 @@
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from tflearn.layers.normalization import batch_normalization
+
+def alexnet(width, height, lr):
+    network = input_data(shape=[None, width, height, 3], name='input')  # Assuming RGB images
+    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = batch_normalization(network)
+    
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = batch_normalization(network)
+    
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = batch_normalization(network)
+    
+    network = fully_connected(network, 4096, activation='relu')
+    network = dropout(network, 0.5)
+    
+    network = fully_connected(network, 4096, activation='relu')
+    network = dropout(network, 0.5)
+    
+    network = fully_connected(network, 3, activation='softmax')
+    network = regression(network, optimizer='adam',
+                         loss='categorical_crossentropy',
+                         learning_rate=lr, name='targets')
+
+    model = tflearn.DNN(network, checkpoint_path='model_alexnet',
+                        max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
+
+    return model
